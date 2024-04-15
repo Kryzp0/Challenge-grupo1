40af8190ec1af081a6cfe916c47518a6bc6b2332
Kryzp0 <chris.jeffry.vega@gmail.com>
2024-04-15 11:56:27 -0400

    Se añade json de zapatillas

---
+++ js/zapatillas.js
@@ -0,0 +1,230 @@
+{
+  "products": [
+    {
+      "id": 1,
+      "name": "Nike Air Max 270",
+      "description": "Zapatillas de running con amortiguación Max Air",
+      "price": 150.00,
+      "image": "https://thelinecl.b-cdn.net/img/products/AH8050-002-1_1024.png",
+      "category": "Running",
+      "color": "Negro/Blanco",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 2,
+      "name": "Nike React Infinity Run Flyknit",
+      "description": "Zapatillas de running diseñadas para reducir lesiones",
+      "price": 160.00,
+      "image": "https://images.stockx.com/images/Nike-React-Infinity-Run-Flyknit-3-White-Metallic-Silver-W.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1652378671",
+      "category": "Running",
+      "color": "Gris/Blanco",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 3,
+      "name": "Nike Air Force 1 '07",
+      "description": "Zapatillas icónicas que ofrecen comodidad y estilo",
+      "price": 100.00,
+      "image": "https://nikeclprod.vtexassets.com/arquivos/ids/160788-800-800?v=637654399741130000&width=800&height=800&aspect=true",
+      "category": "Estilo de vida",
+      "color": "Blanco/Blanco",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 4,
+      "name": "Nike Joyride Run Flyknit",
+      "description": "Zapatillas diseñadas para una sensación única de amortiguación",
+      "price": 180.00,
+      "image": "https://falabella.scene7.com/is/image/Falabella/14794222_1?wid=800&hei=800&qlt=70",
+      "category": "Running",
+      "color": "Azul/Amarillo",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 5,
+      "name": "Nike Air Zoom Pegasus 38",
+      "description": "Zapatillas de running versátiles y cómodas",
+      "price": 120.00,
+      "image": "https://home.ripley.cl/store/Attachment/WOP/D317/2000388872136/2000388872136-2.jpg",
+      "category": "Running",
+      "color": "Azul/Blanco",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 6,
+      "name": "Nike Air Max 97",
+      "description": "Zapatillas de estilo retro con tecnología Max Air",
+      "price": 170.00,
+      "image": "https://cdn-images.farfetch-contents.com/12/37/83/18/12378318_24022887_1000.jpg",
+      "category": "Estilo de vida",
+      "color": "Plateado/Negro",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 7,
+      "name": "Nike Air Zoom SuperRep 2",
+      "description": "Zapatillas de entrenamiento diseñadas para movimientos rápidos",
+      "price": 130.00,
+      "image": "https://falabella.scene7.com/is/image/Falabella/15313345_1?wid=800&hei=800&qlt=70",
+      "category": "Entrenamiento",
+      "color": "Negro/Blanco",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 8,
+      "name": "Nike Air VaporMax 2022",
+      "description": "Zapatillas con amortiguación Air para una sensación ligera y elástica",
+      "price": 200.00,
+      "image": "https://storage.googleapis.com/ssr_uk/products/a29fec3afba34e5558afded7333f2207.jpg",
+      "category": "Running",
+      "color": "Gris/Blanco",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 9,
+      "name": "Nike Air Max Plus",
+      "description": "Zapatillas inspiradas en los años 90 con tecnología Tuned Air",
+      "price": 160.00,
+      "image": "https://nikeclprod.vtexassets.com/arquivos/ids/835697-800-800?v=638327406311330000&width=800&height=800&aspect=true",
+      "category": "Estilo de vida",
+      "color": "Negro/Azul",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 10,
+      "name": "Nike ZoomX Invincible Run Flyknit",
+      "description": "Zapatillas de running con una espuma ligera y reactiva",
+      "price": 180.00,
+      "image": "https://s1.dswcdn.com/uploads/Nike_Other_Shoes/Nike_ZoomX_Invincible_Run_Flyknit_Blue_Orbit_Lime_Glow_Football_Grey_CT2228-401.jpg",
+      "category": "Running",
+      "color": "Azul/Blanco",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 11,
+      "name": "Nike Air Zoom Alphafly NEXT%",
+      "description": "Zapatillas de running diseñadas para competiciones de élite",
+      "price": 275.00,
+      "image": "https://cdn-images.farfetch-contents.com/20/40/46/90/20404690_50336566_1000.jpg",
+      "category": "Competición",
+      "color": "Verde/Negro",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 12,
+      "name": "Nike Air Max 90",
+      "description": "Zapatillas con diseño clásico y comodidad duradera",
+      "price": 120.00,
+      "image": "https://nikeclprod.vtexassets.com/arquivos/ids/271587-800-800?v=637771710351800000&width=800&height=800&aspect=true",
+      "category": "Estilo de vida",
+      "color": "Blanco/Negro",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 13,
+      "name": "Nike Zoom Freak 3",
+      "description": "Zapatillas de baloncesto diseñadas para Giannis Antetokounmpo",
+      "price": 120.00,
+      "image": "https://falabella.scene7.com/is/image/Falabella/126677332_1?wid=800&hei=800&qlt=70",
+      "category": "Baloncesto",
+      "color": "Blanco/Negro",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 14,
+      "name": "Nike Zoom Freak 2",
+      "description": "Zapatillas de baloncesto inspiradas en Giannis Antetokounmpo",
+      "price": 130.00,
+      "image": "https://images-cdn.ubuy.co.id/655530299afbbc74f2107bf5-nike-big-kids-zoom-freak-2-basketball.jpg",
+      "category": "Baloncesto",
+      "color": "Negro/Blanco",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 15,
+      "name": "Nike Renew Run 2",
+      "description": "Zapatillas de running versátiles con amortiguación suave",
+      "price": 90.00,
+      "image": "https://www.runningxpert.com/media/catalog/product/cache/ed1b3c59211d8cf1c9523654e2efd698/n/i/nike_renew_run_3_.jpg",
+      "category": "Running",
+      "color": "Negro/Gris",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 16,
+      "name": "Nike Air Zoom Structure 24",
+      "description": "Zapatillas de running con soporte y estabilidad",
+      "price": 130.00,
+      "image": "https://bikila.com/32375-thickbox_default/nike-air-zoom-structure-24.jpg",
+      "category": "Running",
+      "color": "Azul/Blanco",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 17,
+      "name": "Nike Air Zoom Victory",
+      "description": "Zapatillas de atletismo diseñadas para velocistas",
+      "price": 150.00,
+      "image": "https://image.goat.com/transform/v1/attachments/product_template_pictures/images/041/423/774/original/CD4385_001.png.png?action=crop&width=750",
+      "category": "Atletismo",
+      "color": "Verde/Negro",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 18,
+      "name": "Nike Air Max Excee",
+      "description": "Zapatillas inspiradas en el diseño clásico del Air Max 90",
+      "price": 90.00,
+      "image": "https://nikeclprod.vtexassets.com/arquivos/ids/770691-800-800?v=638253939316500000&width=800&height=800&aspect=true",
+      "category": "Estilo de vida",
+      "color": "Blanco/Azul",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 19,
+      "name": "Nike Air Zoom Tempo NEXT%",
+      "description": "Zapatillas de running para entrenamientos de velocidad",
+      "price": 200.00,
+      "image": "https://i.ebayimg.com/images/g/4EkAAOSwv65hcEzv/s-l1600.jpg",
+      "category": "Running",
+      "color": "Amarillo/Negro",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    },
+    {
+      "id": 20,
+      "name": "Nike Air Max 2090",
+      "description": "Zapatillas con diseño futurista y tecnología Air Max",
+      "price": 150.00,
+      "image": "https://s1.dswcdn.com/uploads/Nike_Air_Max_Shoes/Air_Max_2090/Nike_Air_Max_2090_Black_Wolf_Grey_Anthracite_White_BV9977-001.jpg",
+      "category": "Estilo de vida",
+      "color": "Negro/Gris",
+      "sizes": ["US 7", "US 8", "US 9", "US 10"]
+    }
+  ],
+  "categories": [
+    {
+      "id": 1,
+      "name": "Running"
+    },
+    {
+      "id": 2,
+      "name": "Estilo de vida"
+    },
+    {
+      "id": 3,
+      "name": "Entrenamiento"
+    },
+    {
+      "id": 4,
+      "name": "Competición"
+    },
+    {
+      "id": 5,
+      "name": "Baloncesto"
+    },
+    {
+      "id": 6,
+      "name": "Atletismo"
+    }
+  ]
+}
